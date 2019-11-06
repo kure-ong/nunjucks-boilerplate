bf7ffbf7274cace2abfe0651e4e3316ed6537631
Clement Ong <clement.ong@germs.sg>
2019-11-06 18:32:24 +0800

    update - sass architecture - data function for nunjucks - package.json and npm scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 'use strict';
 
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     concat = require('gulp-concat'),
-    gulpif = require('gulp-if'),
-    del = require('del'),
     cache = require('gulp-cache'),
+    data = require('gulp-data'),
+    gulpif = require('gulp-if'),
     imagemin = require('gulp-imagemin'),
+    modernizr = require('gulp-modernizr'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    postcss = require('gulp-postcss'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    postcss = require('gulp-postcss'),
+    del = require('del'),
     autoprefixer = require('autoprefixer'),
-    modernizr = require('gulp-modernizr'),
-    browserSync = require('browser-sync').create(),
-    nunjucksRender = require('gulp-nunjucks-render');
+    browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
+const rootDir = 'dist',
+    publicDir = 'dist';
 
 gulp.task('clean', function (done) {
   del(['dist/**/*']);
@@ -23,10 +26,11 @@ gulp.task('clean', function (done) {
   done();
 });
 
-// gulp.task('clean:cache', function (done) {
-//   cache.clearAll();
-//   done();
-// });
+// -- uncomment to clear cache for image processing when images seem to not get optimised
+gulp.task('clean:cache', function (done) {
+  cache.clearAll();
+  done();
+});
 
 gulp.task('sass', function () {
   var plugins = [
@@ -37,7 +41,7 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest(publicDir + '/css'))
     .pipe(browserSync.stream());
 });
 
@@ -49,73 +53,98 @@ gulp.task('scripts:vendor', function() {
     'node_modules/imagesloaded/imagesloaded.pkgd.min.js',
   ])
     .pipe(gulpif('!jquery.min.js',concat('bundle.js')))
-    .pipe(gulp.dest('dist/js/vendor'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(publicDir + '/js/vendor'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
     // .pipe(uglify()) //need to install uglify plugin to use
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest(publicDir + '/js'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('modernizr', function() {
   return gulp.src(['src/js/**/*.js', '!src/js/vendor/**'])
     .pipe(modernizr())
-    .pipe(gulp.dest('dist/js/vendor'))
+    .pipe(gulp.dest(publicDir + '/js/vendor'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('nunjucks', function() {
   // Gets .html and .nunjucks files in pages
   return gulp.src('src/pages/**/*.+(html|nunjucks|njk)')
+  //-- Adds data from the JSON to nunjucks
+  // .pipe(data(function() {
+  //   return require('./src/data/images.json')
+  // }))
   // Renders template with nunjucks
   .pipe(nunjucksRender({
       path: ['src/templates']
     }))
   // output files in app folder
-  .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest(rootDir))
   .pipe(browserSync.stream());
 });
 
 gulp.task('img', function() {
 	return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
 		.pipe(cache(
-    // .pipe(
-      imagemin([
-        // Setting interlaced to true
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.jpegtran({progressive: true}),
-        imagemin.optipng({optimizationLevel: 8})
-      ])
+            imagemin([
+                imagemin.gifsicle({interlaced: true}),
+                imagemin.jpegtran({progressive: true}),
+                imagemin.optipng({optimizationLevel: 7}),
+                imagemin.svgo({
+                    plugins: [
+                        {removeViewBox: true},
+                        {cleanupIDs: false}
+                    ]
+                })
+            ], {
+                verbose: true
+            })
+        )
     )
-    )
-		.pipe(gulp.dest('dist/img'));
+		.pipe(gulp.dest(publicDir + '/img'));
+});
+
+gulp.task('copy:fontawesome', function () {
+    return gulp.src([
+        'node_modules/@fortawesome/fontawesome-free/webfonts/**',
+    ],{base: 'node_modules/@fortawesome/fontawesome-free/webfonts/'})
+    .pipe(gulp.dest('src/fonts/'));
 });
 
 gulp.task('copy:files', function () {
   return gulp.src([
     'src/fonts/**/*',
   ],{base: 'src/'})
-    .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest(publicDir))
     .pipe(browserSync.stream());
 });
+
+// gulp.task('copy:rootFiles', function () {
+//   return gulp.src([
+//     'src/.htaccess'
+//   ],{base: 'src/'})
+//     .pipe(gulp.dest(rootDir))
+//     .pipe(browserSync.stream());
+// });
 
 gulp.task('copy:code', function () {
   return gulp.src([
     'src/data/**/*',
     'src/scripts/**/*',
   ],{base: 'src/'})
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(publicDir))
     .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function() {
   browserSync.init({
+      //-- uncomment if you running this from a server and comment out 'server' parameter
       // proxy: "yourlocal.dev",
       server: {
-        baseDir: "dist/"
+        baseDir: publicDir + "/"
       },
       https: true
   });
@@ -125,8 +154,20 @@ gulp.task('serve', function() {
   gulp.watch('src/**/*.(html|nunjucks|njk)', gulp.series('nunjucks'));
   gulp.watch('src/img/**/*.+(jpg|jpeg|gif|png|svg)', gulp.series('img'));
   gulp.watch(['src/fonts/**/*'], gulp.series('copy:files'));
+  // gulp.watch(['src/.htaccess'], gdulp.series('copy:rootFiles'));
   gulp.watch(['src/data/**/*','src/scripts/**/*'], gulp.series('copy:code'));
 });
 
-gulp.task('build', gulp.series('clean','copy:files','copy:code','scripts:vendor','modernizr','scripts','sass','nunjucks','img'));
+gulp.task('build', gulp.series(
+  'clean',
+  // 'copy:rootFiles',
+  'copy:fontawesome',
+  'copy:files',
+  'copy:code',
+  'scripts:vendor',
+  'modernizr',
+  'scripts',
+  'sass',
+  'nunjucks',
+  'img'));
 gulp.task('default', gulp.series('build','serve'));
